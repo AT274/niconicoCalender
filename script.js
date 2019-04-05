7c6b873b46272c4f9bbdeeb8994cfe3046ec9527
山路 友也 <yamaji@yamajinoMacBook-Pro.local>
2019-04-05 11:10:50 +0900

    firsr commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var year, month, day;
 now = new Date();
 year = now.getFullYear();
-month = now.getMonth(); // 0-indexなので注意
+month = now.getMonth(); // 0-indexなので注意!
 day = now.getDate();
 
 var calender = makeCalender(year, month);

60ae79f91f3d4194bd8879b9de64cce2ed0e5502
yamaji <yamaji@fusic.co.jp>
2019-04-08 16:05:16 +0900

    ニコニコマークの書き込み機能追加

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var year, month, day;
-now = new Date();
-year = now.getFullYear();
-month = now.getMonth(); // 0-indexなので注意
-day = now.getDate();
+var now = new Date();
+var year = now.getFullYear();
+var month = now.getMonth(); // 0-indexなので注意
+varday = now.getDate();
+var modal = moldModal();
+
 
 
 contructCalenderMold();
@@ -10,6 +12,7 @@ exeCalenderProcess();  // 初回起動
 
 
 function exeCalenderProcess(){
+    contructCalenderMold();
     var calender = makeCalenderBaseData(year, month);
     showCalenderHeader();
     showCalender(calender);
@@ -20,12 +23,14 @@ function exeCalenderProcess(){
 $('#next-btn').click(function(){ 
     addMonth();
     exeCalenderProcess();
+    $('#modal').hide();
 });
 
 
 $('#prev-btn').click(function(){ 
     subMonth();
     exeCalenderProcess();
+    $('#modal').hide();
 });
 
 
@@ -129,6 +134,7 @@ function contructCalenderMold(){
                 cell.className = 'legend';
                 cell.appendChild(moldCalenderLegend());
             } else {
+                cell.className = 'date'
                 cell.appendChild(moldCalenderContent());
             }
         }
@@ -152,19 +158,74 @@ function moldCalenderContent(){
     var divWrapper = document.createElement('div');
     var divUpper = document.createElement('div');
     var divLower = document.createElement('div')
+    var img = document.createElement('img');
     var pDayNumber = document.createElement('p');
     var pDayText = document.createElement('p');
 
     divWrapper.className = 'content-wrapper';
     divUpper.className = 'upper-part';
-    divLower.className = 'loewr-part';
+    divLower.className = 'lower-part';
+    img.src = "";
+    img.alt = "";
     pDayNumber.className = 'day-number';
     pDayText.className = 'day-text';
 
     divUpper.appendChild(pDayNumber);
     divUpper.appendChild(pDayText);
+    divLower.appendChild(img);
     divWrapper.appendChild(divUpper);
     divWrapper.appendChild(divLower);
 
     return divWrapper;
 }
+
+// モーダルの型
+function moldModal(){
+    var modal = document.createElement('div');
+    var imgSmile = document.createElement('img');
+    var imgNormal = document.createElement('img');
+    var imgSad = document.createElement('img');
+
+    modal.id = 'modal';
+    imgSmile.src = 'img/smile.png'
+    imgNormal.src = 'img/normal.png'
+    imgSad.src = 'img/sad.png'
+
+    modal.appendChild(imgSmile);
+    modal.appendChild(imgNormal);
+    modal.appendChild(imgSad);
+
+    return modal;
+}
+
+// モーダルの表示
+$(document).on('click', '.date', function(){
+        // 先に準備しておかないと、１回目の表示スピードがずれます
+        var cell = this;
+        cell.appendChild(modal);
+
+        // 一度隠す
+        $('#modal').hide();
+        
+        // 日付けが無い部分をクリックしても何も表示しない
+        if ($(this).find('.day-number')[0].innerText == ''){
+            return
+        }
+
+        // クリックされた日付けセルの下にモーダルを表示
+        $('#modal').fadeIn(1000);
+        var position = cell.getBoundingClientRect();;
+        var modalY = position.top + 50;
+        var modalX = position.left - 20;
+        modal.style.top = modalY + 'px';
+        modal.style.left = modalX + 'px';
+    }, 
+);
+
+// ニコニコマークの表示
+$(document).on('click', 'img', function(event){
+　  var src = $(this).attr('src')
+    $('td:has(#modal)').find('img').eq(0).attr('src', src);
+    $('#modal').hide();
+    event.stopPropagation(); // 再表示をブロック
+});
